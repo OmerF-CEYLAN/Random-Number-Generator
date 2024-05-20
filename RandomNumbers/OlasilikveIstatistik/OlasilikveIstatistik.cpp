ad920e47e2b9f8b21747e7958fcda70385889c47
OmerF-CEYLAN <omerfceylan2004@gmail.com>
2024-05-20 13:01:11 +0300

    Random Number Generator
    Generates random numbers in the range of min and max values that are given.

---
+++ RandomNumbers/OlasilikveIstatistik/OlasilikveIstatistik.cpp
@@ -0,0 +1,81 @@
+﻿#include <cstdlib> 
+#include <iostream> 
+#include <time.h>
+
+using namespace std;
+
+
+
+int main()
+{
+
+
+    int min, max, n, random, size, temp,numsBetween;
+
+    size = 1;
+
+    cout << "En kucuk sayiyi giriniz : ";
+
+    cin >> min;
+
+    cout << "En buyuk sayiyi giriniz : ";
+
+    cin >> max;
+
+    cout << "Kac tane sayi olusturmak istersiniz : ";
+
+    cin >> n;
+
+    numsBetween = max - min + 1;
+
+    srand(time(NULL));
+
+    if (numsBetween < n) { // max ve min arasındaki sayı miktarı istediğimiz sayı miktarından az ise tekrar edebilir sayılar üret
+
+
+        for (int i = 0; i < n; i++) {
+
+            random = min + rand() % numsBetween;
+
+            cout << random << endl;
+
+        }
+
+    }
+    else {
+
+        int* repeat = new int[size];
+    
+        for (int i = 0; i < size; i++)
+        {
+            repeat[i] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            random = min + rand() % numsBetween;
+
+            for (int j = 0; j < size;j++) {
+
+                if (random == repeat[j]) {
+                    n++;
+                    break;
+                }
+                    
+                else if ( (j == (size-1)) || (size == 1) ) {
+
+                    repeat[size - 1] = random;
+
+                    cout << random << endl;
+
+                    size++;
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+    return 0;
+}
